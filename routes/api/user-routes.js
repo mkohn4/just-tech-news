4b12be78f5cdf7ee6aba27da1d47f1efddd8af1b
maxkohn <kohn.max@gmail.com>
2021-12-22 13:25:04 -0500

    added post routes and updated relationships

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const {User, Post} = require('../../models');
 
 //GET /api/users
 router.get('/', (req,res) => {
@@ -21,7 +21,13 @@ router.get('/:id', (req,res) => {
         attributes: {exclude: ['password']},
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title','post_url', 'created_at']
+            }
+        ]
     }).then(dbUserData => {
         if(!dbUserData) {
             res.status(404).json({message: 'No user found with this id'});
@@ -73,8 +79,8 @@ router.post('/login', (req,res) => {
 
         //res.json({user: dbUserData});
     
-    })
-})
+    });
+});
 
 //PUT /api/users/1
 router.put('/:id', (req,res) => {
