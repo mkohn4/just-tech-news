5cb90f489fe861a4dce6323b192aed536544db60
maxkohn <kohn.max@gmail.com>
2021-12-25 12:40:58 -0500

    updated user and post routes to include comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 
 // get all users
@@ -18,6 +18,14 @@ router.get('/', (req, res) => {
     order: [['created_at', 'DESC']],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+      },
+        {
         model: User,
         attributes: ['username']
       }
@@ -43,6 +51,14 @@ router.get('/:id', (req, res) => {
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+          },
       {
         model: User,
         attributes: ['username']
