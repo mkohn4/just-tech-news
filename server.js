59699dd158636201638e93b56a667550cb026fa8
maxkohn <kohn.max@gmail.com>
2021-12-29 13:00:12 -0500

    added login routes

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const routes = require('./controllers/index.js');
 const path = require('path');
 const exhbs = require('express-handlebars');
 const hbs = exhbs.create({});
+const session = require('express-session');
 
-
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 //serve up everything in public folder as static resources
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 //setup handlebars as app engine of choice
 app.engine('handlebars', hbs.engine);
