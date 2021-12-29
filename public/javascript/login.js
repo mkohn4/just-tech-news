c7703064716a107562b73783824dd387df301dcd
maxkohn <kohn.max@gmail.com>
2021-12-29 12:51:16 -0500

    updated exta things

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-function signUpFormHandler(event) {
+async function signUpFormHandler(event) {
     event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
@@ -6,7 +6,7 @@ function signUpFormHandler(event) {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && email && password) {
-        fetch('/api/users', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
@@ -14,8 +14,41 @@ function signUpFormHandler(event) {
                 password
             }),
             headers: {'Content-Type': 'application/json'}
-        }).then((response) => {console.log(response)})
+        });
+        console.log(response);
+        //check response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+        console.log(response);
+        //check response status
+        if (response.ok) {
+            console.log('success');
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
 document.querySelector('.signup-form').addEventListener('submit', signUpFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
