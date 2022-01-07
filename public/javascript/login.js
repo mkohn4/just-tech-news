4ecc73e0471fe48bcba66efe1a08b9a3f99ddf22
maxkohn <kohn.max@gmail.com>
2022-01-06 19:01:32 -0500

    added functionality for logged in dashboard

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -43,7 +43,7 @@ async function loginFormHandler(event) {
         //check response status
         if (response.ok) {
             console.log('success');
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
         }
